9bb9ecfd78922c84e7e473a9029389f65706a26d
ObedRav <obedrayo2356@gmail.com>
2023-04-06 14:35:25 +0000

    Adding route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module Name:
+0-hello_route
+
+Module Description:
+This module contains the routers
+
+Module Attributes:
+- None
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def home():
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host="0.0.0.0")
