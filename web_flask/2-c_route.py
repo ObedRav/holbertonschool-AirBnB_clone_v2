712b492aa24d22a54790323d2db8c69c5d463c2d
ObedRav <obedrayo2356@gmail.com>
2023-04-06 14:46:24 +0000

    Adding some routes

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,13 +15,19 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def home():
+def home() -> str:
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
+def hbnb() -> str:
     return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def func_text(text: str) -> str:
+    return f"C {text}"
+
 
 
 if __name__ == '__main__':
